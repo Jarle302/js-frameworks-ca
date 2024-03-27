d48bb0be0434c5ede38459b0cb33a101b97a4606
Jarle302 <jarlehtollaksen2@live.no>
2024-03-27 13:08:34 +0100

    test

--- app/_ui/components/NavBar/index.tsx
+++ app/_ui/components/NavBar/index.tsx
@@ -9,7 +9,9 @@ const NavBar = () => {
   const contactPath = "/contact";
   const [search, setSearch] = useState("");
   const defaultStyle = "text-zinc-200 font-bold hover:text-green-300 ";
-  const activeStyle = `${defaultStyle}  text-green-500 hover:text-green-500`;
+  const [active, setActive] = useState(
+    `${defaultStyle}  text-green-500 hover:text-green-500`
+  );
   console.log(pathname, homePath, contactPath);
   return (
     <nav className={"flex justify-between w-full"}>
@@ -38,7 +40,7 @@ const NavBar = () => {
         <li>
           {" "}
           <Link
-            className={pathname === homePath ? activeStyle : defaultStyle}
+            className={pathname === homePath ? active : defaultStyle}
             href={homePath}>
             Home
           </Link>
@@ -46,7 +48,7 @@ const NavBar = () => {
         <li>
           {" "}
           <Link
-            className={pathname === contactPath ? activeStyle : defaultStyle}
+            className={pathname === contactPath ? active : defaultStyle}
             href={contactPath}>
             Contact Us
           </Link>
